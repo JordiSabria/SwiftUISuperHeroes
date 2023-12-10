0172a80b0a8c150706343f41ceea3ec321996330
Jordi Sabrià Pagès <jordisabria@hotmail.com>
2023-12-10 18:01:05 +0100

    Initial Commit

---
+++ SwiftUISuperHeroes/SwiftUISuperHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISuperHeroesApp.swift
+//  SwiftUISuperHeroes
+//
+//  Created by Jordi Sabrià Pagès on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISuperHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
